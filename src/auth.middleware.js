030e1b566d478270c760e0be3c968f5b7637edb8
0Gravity94 <leesooyun94@gmail.com>
2023-07-05 12:09:23 +0800

    authentication still not working

---
+++ src/auth.middleware.js
@@ -0,0 +1,34 @@
+import JSONtoken from "jsonwebtoken";
+
+export const tokenVerify = (req, res, next) => {
+	const auth = req.header["authorization"];
+
+	if (!auth) {
+		return res.status(400).json({
+			meta: {
+				code: "AUTH-400",
+				message: "Missing jwt",
+			},
+			data: {},
+		});
+	}
+
+	const token = auth.split(" ")[1];
+
+	try {
+		const decode = JSONtoken.verify(token, "silahkan");
+	} catch (error) {
+		return res.status(401).json({
+			meta: {
+				code: "AUTH-401",
+				message: "Invalid bearer",
+			},
+			data: {},
+		});
+	}
+
+	return next(); //lanjut ke middleware selanjutnya
+};
+
+// Client ==> tokenVerify===> fungsiAPI
+// Client ===> tokenVerify ===> Client
